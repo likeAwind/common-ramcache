3b9f0b9a709545ebdb675146053903bc5acf5f37
windforce <windforce@DESKTOP-4J49H1O>
2018-02-12 15:31:45 +0800

    过期增强接口

--- common-ramcache/src/main/java/com/windforce/common/ramcache/enhance/Enhancer.java
+++ common-ramcache/src/main/java/com/windforce/common/ramcache/enhance/Enhancer.java
@@ -5,12 +5,17 @@ import com.windforce.common.ramcache.exception.EnhanceException;
 
 /**
  * 实体类实例增强器接口
+ * 
+ * 实践中发现,所带来的便利并没有增加多少.反而多余的增强耗费过多的内存.并且增加复杂度.
+ * 
  * @author frank
  */
+@Deprecated
 public interface Enhancer {
 
 	/**
 	 * 将指定实体的类实例转换为增强类实例
+	 * 
 	 * @param entity
 	 * @return
 	 * @throws EnhanceException
